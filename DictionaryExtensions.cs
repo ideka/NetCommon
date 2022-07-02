a8c779a6d2a42b1abe950084934a33735e218c6f
Gerardo Marset <gammer1994@gmail.com>
2022-07-02 15:59:02 -0300

    Add MergeOverwrite for dictionaries

--- DictionaryExtensions.cs
+++ DictionaryExtensions.cs
@@ -4,6 +4,14 @@ namespace Ideka.NetCommon
 {
     public static class DictionaryExtensions
     {
+        public static IDictionary<TKey, TValue> MergeOverwrite<TKey, TValue>(this IDictionary<TKey, TValue> source,
+            IDictionary<TKey, TValue> other)
+        {
+            foreach (var (key, value) in other)
+                source[key] = value;
+            return source;
+        }
+
         public static void Deconstruct<TK, TV>(this KeyValuePair<TK, TV> pair, out TK key, out TV value)
         {
             key = pair.Key;
