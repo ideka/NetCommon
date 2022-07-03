c96ead4d77dcdd2c84d5b0911f8acbf683927a4c
Gerardo Marset <gammer1994@gmail.com>
2022-07-03 00:01:35 -0300

    Add Format helper method for strings

---
+++ StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Ideka.NetCommon
+{
+    public static class StringExtensions
+    {
+        public static string Format(this string format, params object[] args)
+            => string.Format(format, args);
+    }
+}
