237c089e3c091bfe7d94145ba042e192aeefabb6
Gerardo Marset <gammer1994@gmail.com>
2023-02-03 03:56:28 -0300

    Add DisposableCollection, WhenDisposed

---
+++ DisposableCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ideka.NetCommon
+{
+    public class DisposableCollection : IDisposable
+    {
+        private readonly HashSet<IDisposable> _disposables = new();
+
+        public T Add<T>(T disposable) where T : notnull, IDisposable
+        {
+            _disposables.Add(disposable);
+            return disposable;
+        }
+
+        public void Dispose()
+        {
+            foreach (var disposable in _disposables)
+                disposable.Dispose();
+
+            _disposables.Clear();
+        }
+    }
+}
