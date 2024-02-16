120513fbef096a01a5302da05a584fc21fddb09f
Gerardo Marset <gammer1994@gmail.com>
2024-02-16 17:25:02 +0000

    Add SelectRecursive

--- EnumerableExtensions.cs
+++ EnumerableExtensions.cs
@@ -62,4 +62,21 @@ public static class EnumerableExtensions
 
         return most;
     }
+
+    public static IEnumerable<T> SelectRecursive<T>(this IEnumerable<T> subjects, Func<T, IEnumerable<T>> selector)
+    {
+        if (subjects == null)
+            yield break;
+
+        var stillToProcess = new Queue<T>(subjects);
+
+        while (stillToProcess.Count > 0)
+        {
+            T item = stillToProcess.Dequeue();
+            yield return item;
+
+            foreach (T child in selector(item))
+                stillToProcess.Enqueue(child);
+        }
+    }
 }
