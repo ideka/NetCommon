dc6ffb1d61dd0a84d60a5d331494c3e1933b42c5
Gerardo Marset <gammer1994@gmail.com>
2024-05-03 03:13:00 +0100

    Add OrderedDictionary

---
+++ MaybeNullWhenAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Diagnostics.CodeAnalysis;
+
+/// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter may be null even if the corresponding type disallows it.</summary>
+/// <remarks>Initializes the attribute with the specified return value condition.</remarks>
+/// <param name="returnValue">
+/// The return value condition. If the method returns this value, the associated parameter may be null.
+/// </param>
+[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+internal sealed class MaybeNullWhenAttribute(bool returnValue) : Attribute
+{
+    /// <summary>Gets the return value condition.</summary>
+    public bool ReturnValue { get; } = returnValue;
+}
