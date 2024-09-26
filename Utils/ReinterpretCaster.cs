4f370fff454d1401f2537ebcb06f8143ad11fab8
Gerardo Marset <gammer1994@gmail.com>
2024-09-26 13:14:11 +0100

    Add ReinterpretCaster

---
+++ Utils/ReinterpretCaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ideka.NetCommon;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct ReinterpretCaster<TFrom, TTo>(TFrom from)
+{
+    [FieldOffset(0)]
+    public TFrom From = from;
+    [FieldOffset(0)]
+    public TTo To;
+
+    public static TTo Cast(TFrom from)
+        => new ReinterpretCaster<TFrom, TTo>(from).To;
+}
